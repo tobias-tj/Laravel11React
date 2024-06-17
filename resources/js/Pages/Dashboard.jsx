258a7573dec953aa92ca645e7891281a6b5fb1af
Tobias Jara <tobiasjara@MacBook-Pro-de-Tobias.local>
2024-06-17 19:20:48 -0400

    Static fix

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -61,7 +61,7 @@ export default function Dashboard({
         </div>
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900">
+            <div className="p-6 text-gray">
               <h3 className="text-gray text-xl font-semibold">
                 My Active Tasks
               </h3>
@@ -80,7 +80,7 @@ export default function Dashboard({
                     <tr key={task.id}>
                       <td className="px-3 py-2">{task.id}</td>
                       <td className="px-3 py-2">
-                        <Link href={route("project.show", task.project.id)} className="font-semibold hover:underline">
+                        <Link href={route("project.show", task.project.id)} className="hover:underline">
                           {task.project.name}
                         </Link>
                       </td>
